c3bc8f176f214b331ee697be56d351c3fd8ef353
Alex Ung <alexung.ucsb@gmail.com>
2016-03-20 21:09:06 -0700

    delete some comments

--- tripping.rb
+++ tripping.rb
@@ -9,7 +9,7 @@ testProperty = {
 "248,109,138,227,104,207,163,119,249,261,162,286,235,205,210,215,259,227,203,183,181,153,140,258,103,198,253,286,254,133,202,142,163,261,205,133,113,152,130,193,153,140,174,282,128,268,114,199,168,254,296,267,224,249,200,207,160,124,108,165,259,293,143,282,267,129,114,268,249,186,255,124,161,247,297,100,113,170,201,239,283,180,142,220,105,294,226,228,101,108,104,187,238,251,106,259,262,120,174,141,231,207,270,193,292,121,250,166,171,287,220,142,176,195,180,215,155,243,116,249,265,164,172,213,151,132,215,235,116,181,260,199,203,189,251,124,128,152,128,188,140,273,176,208,143,143,108,265,269,273,265,182,275,237,188,183,108,234,137,270,106,273,122,105,171,236,213,278,281,102,117,163,277,104,294,252,210,124,203,253,100,220,205,226,100,236,101,150,148,104,205,110,249,175,131,185,210,262,290,271,214,247,158,210,220,156,137,284,245,212,137,237,235,112,158,278,127,132,272,104,103,274,199,260,154,209,186,260,251,247,180,106,107,102,164,290,166,243,234,244,108,182,249,118,99,131,191,231,247,152,120,146,154,223,209,111,116,194,101,212,163,110,257,196,230,250,273,152,116,185,158,276,293,285,219,101,119,159,170,168,147,104,211,137,235,136,136,152,281,176,134,144,130,298,269,272,101,168,141,198,184,154,186,109,230,185,195,282,265,210,290,265,194,120,122,240,256,280,289,164,109,136,213,267,161,265,162,162,182,196,251,204,191,280,199,243,214,255,180,183,200,114,250,159,267,217,213,263,151,115,223,221,244,261,190,166,112,266,291,283,249,135,102,151,113"
 }
 
-# print testProperty[:price]
+#print testProperty[:price]
 #availability, minstay, price all have length of 365. 1 per each day of year
 #N is not available
 #Y is available
@@ -32,6 +32,7 @@ def available_ranges(property)
   @minstay = property[:minstay].split(',')
   @tempArr = []
 
+  # iterate through @availability
   @availability.each_with_index do |avail, i|
     # finding the current date based on every iteration
     @currentDate = (Date.parse(property[:start_date]) + i).strftime("%Y-%m-%d")
@@ -63,30 +64,9 @@ def available_ranges(property)
       @tempArr = []
     end
   end
-  # instantiate tempArr = []
-
-  # for loop through property.availability
-    # instantiate currentDate = Date.today + index
-
-    # if availability of this day is Y
-      # if tempArr.length == 0
-        # tempArr.push(currentDate)
-
-      # if tempArr.length == 1
-        # tempArr.push(currentDate)
-
-      # if tempArr.length == 2
-        # tempArr.pop()
-        # tempArr.push(currentDate)
-    # else
-      # @result.push(tempArr)
-      # tempArr = [] empty arr
 
   return @result
-  #return @pass
 end
-
-#2015-03-27 is last date
 
 print available_ranges(testProperty)
 
